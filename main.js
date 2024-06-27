42f3e706c3c918ff585963d4dc914b8bbbd06a82
Ketlyn Poltronhere <106679371+Poltket@users.noreply.github.com>
2024-06-27 08:32:53 -0300

    Aula 27/06

--- main.js
+++ main.js
@@ -40,3 +40,12 @@ const perguntas = [
         ]
     },
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
