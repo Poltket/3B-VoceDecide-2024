4888976bd6e8816250812af78bf0cf8a89141cf5
Ketlyn Poltronhere <106679371+Poltket@users.noreply.github.com>
2024-07-24 08:45:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,35 +5,35 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
 const perguntas = [
     {
-        enunciado: "Pergunta 01",
+        enunciado: "Ryan é um garotinho de 4 anos, que possui autismo moderado (Nível 2), e sua mãe está a procura de uma escolhinha para matricula-lô. Você acha que ele deveria ser matriculado em uma escola de ensino regular ou em uma escola especializada em educação para crianças autistas?",
+        alternativa: [
+            "Ryan deve ser matriculado em uma escola especializada em educação para crianças que possuem autismo, para que suas necesidades sejam atendidas corretamente.",
+            "Ryan deve ser matriculado em uma escola de ensino regular, pois ele é uma criança como qualquer outra."
+        ]
+    },
+    {
+        enunciado: "Ryan completou 12 anos e sua mãe percebeu que ele tinha uma boa audição e gostava de escutar música, decidiu colocá-lo em uma escola de música, que instrumento Ryan deveria tocar?",
+        alternativa: [
+            "Flauta, pois é o instrumento favorito de Ryan, mas ele tem bastante dificuldade para tocar.",
+            "Piano, pois ele tem facilidade em tocar, porém não é seu intrumento favorito."
+        ]
+    },
+    {
+        enunciado: "Com 19 anos, Ryan conheceu Isadora, uma garota muito bondoza que cuidava de crianças autistas, Ryan queria chamâ-la para sair, mas seus amigos disseram que não uma boa ideia, o que Ryan deve fazer?",
         alternativa: [
             "Alternativa 01",
             "Alternativa 02"
         ]
     },
     {
-        enuncado: "Pergunta 02",
+        enunciado: "Pergunta 04",
         alternativa: [
             "Alternativa 01",
             "Alternativa 02"
         ]
     },
     {
-        enuncado: "Pergunta 03",
-        alternativa: [
-            "Alternativa 01",
-            "Alternativa 02"
-        ]
-    },
-    {
-        enuncado: "Pergunta 04",
-        alternativa: [
-            "Alternativa 01",
-            "Alternativa 02"
-        ]
-    },
-    {
-        enuncado: "Pergunta 05",
+        enunciado: "Pergunta 05",
         alternativa: [
             "Alternativa 01",
             "Alternativa 02"
@@ -67,12 +67,14 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoselecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + "";
     atual++;
     mostraPergunta();
 }
 
 function mostraResultado (){
     caixaPerguntas.textContent = "Aqui vai um texto"
-    texto.caixaResultado.textContent = história;
+    textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
