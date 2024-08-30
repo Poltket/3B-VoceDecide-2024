102e1d6dfff652560b259508b41aaf362bc6c698
Ketlyn Poltronhere <106679371+Poltket@users.noreply.github.com>
2024-08-30 08:12:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const perguntas = [
         enunciado: " Ryan é um garotinho de 4 anos, que possui autismo moderado (Nível 2), e sua mãe está a procura de uma escolinha para matriculá-lo. Você acha que ele deveria ser matriculado em uma escola de ensino regular ou em uma escola especializada em educação para crianças autistas?",
         alternativas: [
             {
-                texto:" Ryan deve ser matriculado em uma escola especializada em educação de crianças que possuem autismo, para que suas necessidades sejam atendidas corrente.",
+                texto:" Ryan deve ser matriculado em uma escola especializada em educação de crianças que possuem autismo, para que suas necessidades sejam atendidas corretamente.",
                 afirmacao: "Ryan foi matriculado em uma escola especializada em educação para crianças que possuem autismo e se adaptou muito bem."
             },
             {
@@ -22,12 +22,12 @@ const perguntas = [
         enunciado: "Ryan completou 12 anos e sua mãe percebeu que ele tinha uma boa audição e gostava de escutar música, então, decidiu colocá-lo em uma escola de música, que intrumento Ryan deveria tocar?",
         alternativas: [
             {
-                texto: "Flauta, pois é o instrumento favorito Ryan, mas ele tem bastante dificuldade em tocar.",
+                texto: "Flauta, pois é o instrumento favorito de Ryan, mas ele tem bastante dificuldade em tocar.",
                 afirmacao: "Com 12 anos ele foi colocado em uma escola de música e começou a tocar flauta, e mesmo com muita dificuldade no aprendizado ele não desistiu."
             },
             {
                 texto: "Piano, pois ele tem facilidade em tocar, porém não é seu instrumento favorito.",
-                afirmacao: "Com 12 anos ele começou a participar da aulas de piano, ele tocava muito bem, porém, não era o instrumento que ele gostaria de tocar."
+                afirmacao: "Com 12 anos ele começou a participar das aulas de piano, ele tocava muito bem, porém, não era o instrumento que ele gostaria de tocar."
             }
         ]
     },
@@ -35,11 +35,11 @@ const perguntas = [
         enunciado: "Com 19 anos, Ryan conheceu Isadora, uma garota muito bondoza que cuidava de crianças autistas, Ryan queria chamá-la para sair, mas seus amigos disseram que não era uma boa ideia, o que Ryan deve fazer?",
         alternativas: [
             {
-                texto: "Ryan não deve ouvir seus amigos e deve chamá-la para sair já que gosta dela.",
-                afirmacao: "Passado alguns anos, quando Ryan completou 19 anos resolveu chamar a garota que conheceu para sair, ele se entendiam muito bem e passaram um ótimo dia juntos."
+                texto: "Ryan não deve ouvir seus amigos e deve chamá-la para sair, já que gosta dela.",
+                afirmacao: "Passado alguns anos, quando Ryan completou 19 anos resolveu chamar a garota que conheceu para sair, eles se entendiam muito bem e passaram um ótimo dia juntos."
             },
             {
-                texto: "Ryan deve ouvir seu amigos e não chamá-la para sair.",
+                texto: "Ryan deve ouvir seus amigos e não chamá-la para sair.",
                 afirmacao: "Passado alguns anos, Ryan ouviu seus amigos e decidiu não chamar a garota que conheceu para sair, ele acreditou que não era o momento certo para se relacionar com alguém. "
             }
         ]
@@ -53,7 +53,7 @@ const perguntas = [
             },
             {
                 texto: " Não, uma simples palestra não pode mudar a vida de ninguém.",
-                afirmacao: "Com 25 anos, Ryan rejeitou a proposta para palestrar sobre o austimo e a educação inclusiva, por seu uma pessoa autista ele não acreditava em si mesmo, e que poderia ter capacidade para enfrentar novos desafios e oportunidades."
+                afirmacao: "Com 25 anos, Ryan rejeitou a proposta para palestrar sobre o austimo e a educação inclusiva, por ser uma pessoa autista ele não acreditava em si mesmo, e que poderia ter capacidade para enfrentar novos desafios e oportunidades."
             }
         ]
     },
